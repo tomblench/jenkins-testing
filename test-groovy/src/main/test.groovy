d9c5c6c42b8ab5534b686d3909b2ce5b5a977981
Tom Blench <tblench@uk.ibm.com>
2017-09-08 12:36:24 +0100

    Add jenkinsfile

--- test-groovy/src/main/test.groovy
+++ test-groovy/src/main/test.groovy
@@ -6,20 +6,38 @@ import org.eclipse.jgit.lib.TagBuilder;
 import org.eclipse.jgit.lib.*;
 import org.eclipse.jgit.storage.file.*;
 import org.eclipse.jgit.revwalk.*;
+import org.eclipse.jgit.api.*;
 
 print "hello world"
 
 def builder = new FileRepositoryBuilder();
-def repository = builder.setGitDir(new File("."))
+def repository = builder.setGitDir(new File("/Users/tomblench/cloudant_dev/jenkins-testing/.git"))
         .readEnvironment() // scan environment GIT_* variables
         .findGitDir() // scan up the file system tree
         .build();
 
+//print repository
+
 def head = repository.resolve("HEAD");
 
-//Ref HEAD = repository.getRef("refs/heads/master");
+//print head
+
+Ref HEAD = repository.getRef("refs/heads/master");
+
+//print HEAD
 
 RevWalk walk = new RevWalk(repository);
+
+//print walk
+
 RevCommit commit = walk.parseCommit(head);
 
-new TagBuilder().setObjectId(commit)
+print "commit ${commit}"
+
+def tc = new TagCommand(repository)
+
+tc.setObjectId(commit).
+   setMessage("testing 123").
+   setName("floof").
+        call()
+
